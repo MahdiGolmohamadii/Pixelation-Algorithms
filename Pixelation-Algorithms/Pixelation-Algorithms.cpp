a812151fb810d94ce3577b090ee61a71fe71145a
MahdiGolmohamadii <mmahdigolmohamadi@gmail.com>
2023-10-15 13:59:33 +0330

    Implemented Avraging Pixels algorithm

--- Pixelation-Algorithms/Pixelation-Algorithms.cpp
+++ Pixelation-Algorithms/Pixelation-Algorithms.cpp
@@ -5,9 +5,29 @@
 using namespace cv;
 using namespace std;
 
+
+
+void avragingPixels(Mat& img, int blocksize) {
+
+    for (int i = 0; i < img.cols; i+=blocksize) {
+        for (int j = 0; j < img.rows; j+=blocksize) {
+            Rect rect(i,j,blocksize,blocksize);
+            Scalar colorAvg = mean(img(rect));
+            rectangle(img, rect,colorAvg,FILLED);
+        }
+    }
+
+
+}
+
+
+
 int main() {
-    Mat img = imread("sample.jpg");
-    imshow("Test Image", img);
+    Mat img = imread("Tupac.jpg");
+    imshow("Normal", img);
+
+    avragingPixels(img,5);
+    imshow("avragingPixels", img);
     waitKey(0);
     return 0;
 }
