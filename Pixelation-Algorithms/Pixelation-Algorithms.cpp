a1270f19a2b7ef5a8dc80194dbeb7e10c5d14b24
MahdiGolmohamadii <mmahdigolmohamadi@gmail.com>
2023-10-15 14:31:30 +0330

    Added sampling algorithm

--- Pixelation-Algorithms/Pixelation-Algorithms.cpp
+++ Pixelation-Algorithms/Pixelation-Algorithms.cpp
@@ -16,8 +16,11 @@ void avragingPixels(Mat& img, int blocksize) {
             rectangle(img, rect,colorAvg,FILLED);
         }
     }
+}
 
-
+void smapling(Mat& srcimage, Mat& dstImage, int fact) {
+    resize(srcimage, dstImage, Size(srcimage.cols/ fact, srcimage.rows/fact));
+    resize(dstImage,dstImage,Size(srcimage.cols,srcimage.rows),0,0, INTER_NEAREST);
 }
 
 
@@ -28,6 +31,10 @@ int main() {
 
     avragingPixels(img,5);
     imshow("avragingPixels", img);
+
+    Mat dst;
+    smapling(img,dst,5);
+    imshow("Sampling", dst);
     waitKey(0);
     return 0;
 }
